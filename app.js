5b5ed53b4751e7112fe3e63315dbc0db0928423c
Jeff Zhong <jzhong@figure.com>
2023-08-10 16:05:33 -0700

    work: using actions/core for debugging

--- app.js
+++ app.js
@@ -1,67 +1,70 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const packageInfo = require('./package.json')
-console.log(`Starting ${packageInfo.name}`)
+const packageInfo = require('./package.json');
+core.info(`Starting ${packageInfo.name}`);
 
-const helpers = require('./helpers')
-const uniq = require('lodash.uniq')
+const helpers = require('./helpers');
+const uniq = require('lodash.uniq');
 
 //require octokit rest.js
 //more info at https://github.com/octokit/rest.js
-const github = require('@actions/github')
-const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+const github = require('@actions/github');
+const core = requie('@actions/core');
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
-let baseDirectories = ''
-if (process.env.BASE_DIRS) baseDirectories = `(?:${process.env.BASE_DIRS})\/`
+let baseDirectories = '';
+if (process.env.BASE_DIRS) baseDirectories = `(?:${process.env.BASE_DIRS})\/`;
 
 //set eventOwner and eventRepo based on action's env variables
-const eventOwnerAndRepo = process.env.GITHUB_REPOSITORY
-const eventOwner = helpers.getOwner(eventOwnerAndRepo)
-const eventRepo = helpers.getRepo(eventOwnerAndRepo)
+const eventOwnerAndRepo = process.env.GITHUB_REPOSITORY;
+const eventOwner = helpers.getOwner(eventOwnerAndRepo);
+const eventRepo = helpers.getRepo(eventOwnerAndRepo);
 
 async function prMonorepoRepoLabeler() {
   try {
     //read contents of action's event.json
-    const eventData = await helpers.readFilePromise(process.env.GITHUB_EVENT_PATH)
+    const eventData = await helpers.readFilePromise(process.env.GITHUB_EVENT_PATH);
 
     if (eventData) {
-      const eventJSON = JSON.parse(eventData)
+      const eventJSON = JSON.parse(eventData);
 
       //set eventAction and eventIssueNumber
-      eventAction = eventJSON.action
-      eventIssueNumber = eventJSON.pull_request.number
+      eventAction = eventJSON.action;
+      eventIssueNumber = eventJSON.pull_request.number;
 
       //get list of files in PR
-      const prFiles = await helpers.listFiles(octokit, eventOwner, eventRepo, eventIssueNumber)
+      const prFiles = await helpers.listFiles(octokit, eventOwner, eventRepo, eventIssueNumber);
 
-      // get list of labels currently on PR
-      const existingLabels = await helpers.listLabelsOnIssue(octokit, eventOwner, eventRepo, eventIssueNumber)
+      //get list of labels currently on PR
+      const existingLabels = await helpers.listLabelsOnIssue(octokit, eventOwner, eventRepo, eventIssueNumber);
+      core.debug('existing labels', exitingLabels);
 
       //get monorepo repo for each file
-      prFilesRepos = prFiles.map(({ filename }) => helpers.getMonorepo(baseDirectories, filename))
+      prFilesRepos = prFiles.map(({ filename }) => helpers.getMonorepo(baseDirectories, filename));
 
       //reduce to unique repos
-      const prFilesReposUnique = uniq(prFilesRepos)
+      const prFilesReposUnique = uniq(prFilesRepos);
 
-      // remove unaffected repos, and add label for each monorepo repo
+      //remove unaffected repos, and add label for each monorepo repo
       //add label for each monorepo repo
       existingLabels.forEach((label) => {
         if (prFilesReposUnique.indexOf(label.name) > -1) {
-          const repoLabel = helpers.getLabel(repo)
-          console.log(`labeling repo: ${repoLabel}`)
+          const repoLabel = helpers.getLabel(repo);
+          core.info(`labeling repo: ${repoLabel}`);
 
-          helpers.addLabel(octokit, eventOwner, eventRepo, eventIssueNumber, repoLabel)
+          helpers.addLabel(octokit, eventOwner, eventRepo, eventIssueNumber, repoLabel);
         } else {
-          helpers.removeLabel(octokit, eventOwner, eventRepo, eventIssueNumber, label.name)
+          core.info(`removing label ${label.name}`);
+          helpers.removeLabel(octokit, eventOwner, eventRepo, eventIssueNumber, label.name);
         }
-      })
+      });
     }
   } catch (error) {
-    console.log(error)
+    core.setFailed(error);
   }
 }
 
 //run the function
-prMonorepoRepoLabeler().catch(console.error)
+prMonorepoRepoLabeler().catch(console.error);
 
-module.exports.prMonorepoRepoLabeler = prMonorepoRepoLabeler
+module.exports.prMonorepoRepoLabeler = prMonorepoRepoLabeler;
